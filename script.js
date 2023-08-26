61d185c3b1bfaa46980e497fa7e947d45055e5d2
sidarth-23 <sidarth157@gmail.com>
2023-08-26 13:23:04 +0530

    Added sass, localStorage and completed the given task

--- script.js
+++ script.js
@@ -1,46 +1,126 @@
 // Using localStorage
-localStorage.setItem('userScore', '0');
-localStorage.setItem('compScore', '0');
+const storedUserScore = localStorage.getItem("userScore")
+  ? localStorage.getItem("userScore")
+  : localStorage.setItem("userScore", "0")
+const storedCompScore = localStorage.getItem("userScore")
+  ? localStorage.getItem("compScore")
+  : localStorage.setItem("compScore", "0")
+const body = document.querySelector("body")
 
 // Setting json for rock, paper, scissor;
 const data = {
   rock: {
     winning: "scissor",
     losing: "paper",
-    "icon-img": '<i class="fa-solid fa-hand-fist fa-4x"></i>',
+    iconImg: '<i class="fa-solid fa-hand-fist fa-4x"></i>',
   },
   paper: {
     winning: "rock",
     losing: "scissor",
-    "icon-img": '<i class="fa-solid fa-hand fa-4x"></i>',
+    iconImg: '<i class="fa-solid fa-hand fa-4x"></i>',
   },
   scissor: {
     winning: "paper",
     losing: "rock",
-    "icon-img": '<i class="fa-solid fa-hand-peace fa-4x"></i>',
+    iconImg: '<i class="fa-solid fa-hand-peace fa-4x"></i>',
   },
 }
 
-// Elements selected
-const body = document.querySelector("body")
-const playingBody = document.querySelector(".main-body")
-const results = document.querySelector('.results');
-const scoreUser = document.querySelector("#scored-user")
-const scoreComp = document.querySelector("#scored-comp")
-const userChoice = document.querySelectorAll(".choose-button")
-const playAgain = document.querySelector(".reset-btn")
-const popup = document.querySelector(".popup")
-const popupExit = document.querySelector(".exit-btn")
-const rulesBtn = document.querySelector("#rules-btn")
-const userAnswer = document.getElementsByClassName("user-pick")
-const compAnswer = document.getElementsByClassName("comp-pick")
-
 // Checking functions
-// const checkWinner = (user, comp) => {
-
-// }
+const pcChoiceArray = ["rock", "paper", "scissor"]
+const checkWinner = (user, comp) => {
+  if (data[user].winning === comp) {
+    return {
+      winnerTag: "winner-user",
+      winText: "Win",
+      isHidden: "",
+    }
+  } else if (user === comp) {
+    return {
+      winnerTag: "",
+      winText: "Tied",
+      isHidden: "hidden",
+    }
+  }
+  return {
+    winnerTag: "winner-comp",
+    winText: "Lose",
+    isHidden: "",
+  }
+}
 
 // Components of page
+const mainpageTxt = () => {
+  return `<div class="main-body">
+      <header>
+        <div class="title">
+          <h1>Rock <br />Paper <br />Scissors</h1>
+        </div>
+        <div class="score-board">
+          <div class="score-tag">
+            <p class="score-player">Computer <br />Score</p>
+            <p class="score-number scored-comp" id="scored-comp">${localStorage.getItem(
+              "compScore"
+            )}</p>
+          </div>
+          <div class="score-tag">
+            <p class="score-player">Your <br />Score</p>
+            <p class="score-number scored-user" id="scored-user">${localStorage.getItem(
+              "userScore"
+            )}</p>
+          </div>
+        </div>
+      </header>
+            <main>
+        </main>
+              <footer>
+        <div class="footer-container">
+          <button class="footer-btn rules-btn" id="rules-btn">Rules</button>
+        </div>
+      </footer>`
+}
+
+const endPageTxt = `    <div class="main-body">
+      <div class="img-container">
+        <img src="assets/prize-icon.svg" alt="Prize" class="prize" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-1" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-2" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-3" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-4" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-5" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-6" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-7" />
+        <img src="assets/star-icon.svg" alt="Star" class="star-8" />
+      </div>
+      <div class="won-end">
+        <h2>Hurray!!</h2>
+        <p>You won the game</p>
+      </div>
+      <button class="win-reset-btn">Play Again</button>
+      <div class="footer-container">
+        <button class="footer-btn rules-btn" id="rules-btn">Rules</button>
+      </div>
+    </div>`
+
+const optionTxt = `<div class="main-container page-1">
+          <div class="choose-option">
+            <div class="options-bg">
+              <img src="assets/Simple_triangle.svg" alt="" />
+            </div>
+            <div class="choose-buttons">
+              <button class="choose-button rock" id="rock">
+                <i class="fa-solid fa-hand-fist fa-4x"></i>
+              </button>
+              <button class="choose-button paper" id="paper">
+                <i class="fa-solid fa-hand fa-4x"></i>
+              </button>
+              <button class="choose-button scissor" id="scissor">
+                <i class="fa-solid fa-hand-peace fa-4x"></i>
+              </button>
+            </div>
+          </div>
+        </div>`
+
 const popupTxt = `<div class="popup">
       <button class="exit-btn">X</button>
       <div class="rules-content">
@@ -64,55 +144,146 @@ const popupTxt = `<div class="popup">
         </ul>
       </div>
     </div>`
-    
-const resultsPageContent = (winner) => {
-  let winnerTag = "";
-  let winText = "Tie";
-  let isHidden = '';
-  if (winner === "comp") {
-    winnerTag = "winner-comp";
-    winText = "Lose";
-  } else if (winner === 'user') {
-    winnerTag = 'winner-user';
-    winText = 'Win';
-  }
 
-  return `      <div class="results">
-        <div class="winner ${winnerTag}">
+const playAgainBtn = `<button class="reset-btn">Play Again</button>`
+
+const resetBtnContainer = `<div class="footer-container">
+          <button class="footer-btn rules-btn" id="rules-btn">Rules</button>
+        </div>`
+
+const nextBtnTxt = `<button class="footer-btn next-btn" id="next-btn">Next</button>`
+
+const resultsPageContent = (userAns, pcAns, winner) => {
+  const tempWin = winner
+
+  return `<div class="main-container page-2">
+  <div class="results">
+        <div class="winner ${tempWin.winnerTag} ${tempWin.isHidden}">
           <div class="winner-c1">
             <div class="winner-c2"></div>
           </div>
         </div>
         <div class="pick user-pick scissor">
           <p>You picked</p>
-          <i class="fa-solid fa-hand-peace fa-4x"></i>
+          ${data[userAns].iconImg}
         </div>
         <div class="pick-result">
           <h2>
-            You <span class="conclude">${winText}</span>
+            You <span class="conclude">${tempWin.winText}</span>
           </h2>
-          <p class="${isHidden}">Against Pc</p>
+          <p class="${tempWin.isHidden}">Against Pc</p>
           <button class="reset-btn">Play Again</button>
         </div>
         <div class="pick comp-pick rock">
           <p>Pc Picked</p>
-          <i class="fa-solid fa-hand-fist fa-4x"></i>
+          ${data[pcAns].iconImg}
         </div>
+      </div>
       </div>`
 }
 
+function initialPageRender() {
+  const mainpage = document.querySelector(".main-body")
+  if (mainpage) {
+    mainpage.remove()
+  }
+  body.insertAdjacentHTML("afterbegin", mainpageTxt())
+  document.querySelector("main").insertAdjacentHTML("afterbegin", optionTxt)
+}
+
+function resultsPageRender(resultsPageText) {
+  document.querySelector(".page-1").remove()
+  document
+    .querySelector("main")
+    .insertAdjacentHTML("afterbegin", resultsPageText)
+  document
+    .querySelector(".footer-container")
+    .insertAdjacentHTML("beforeend", nextBtnTxt)
+}
+
+function resetRender() {
+  document.querySelector(".page-2").remove()
+  document.querySelector("main").insertAdjacentHTML("beforeend", optionTxt)
+  const nextBtn = document.querySelector(".next-btn")
+  nextBtn.remove()
+}
+
+function nextWinner(winCheck) {
+  if (winCheck.winText === "Win") {
+    localStorage.setItem(
+      "userScore",
+      String(Number(localStorage.getItem("userScore")) + 1)
+    )
+    document.querySelector(".main-body").remove()
+    body.insertAdjacentHTML("afterbegin", endPageTxt)
+  } else if (winCheck.winText === "Lose") {
+    localStorage.setItem(
+      "compScore",
+      String(Number(localStorage.getItem("compScore")) + 1)
+    )
+    resetRender()
+    document.querySelector("#scored-user").textContent =
+      localStorage.getItem("userScore")
+    document.querySelector("#scored-comp").textContent =
+      localStorage.getItem("compScore")
+  } else {
+    resetRender()
+    document.querySelector("#scored-user").textContent =
+      localStorage.getItem("userScore")
+    document.querySelector("#scored-comp").textContent =
+      localStorage.getItem("compScore")
+  }
+}
+
+initialPageRender()
+
+// Elements selected
+const mainBody = document.getElementsByClassName("main-body")[0]
+const scoreUser = document.getElementsByClassName("scored-user")[0]
+const scoreComp = document.getElementsByClassName("scored-comp")[0]
+
+const footerContainer = document.getElementsByClassName("footer-container")[0]
 
 // Button events
-rulesBtn.addEventListener("click", function (e) {
-  e.preventDefault()
-  playingBody.insertAdjacentHTML("beforeend", popupTxt)
-})
+let winnerCheck
+
 body.addEventListener("click", function (e) {
+  e.preventDefault()
+  let userSelect, compSelect, resultPage
+
+  if (e.target.classList.contains("rules-btn")) {
+    document
+      .querySelector(".main-body")
+      .insertAdjacentHTML("beforeend", popupTxt)
+  }
+
   if (e.target.classList.contains("exit-btn")) {
-    e.preventDefault()
     const popup = document.querySelector(".popup")
     if (popup) {
       popup.remove()
     }
+  }
+
+  if (
+    e.target.classList.contains("choose-button") ||
+    e.target.closest("button").classList.contains("choose-button")
+  ) {
+    userSelect = e.target.closest("button").getAttribute("id")
+    compSelect = pcChoiceArray[Math.floor(Math.random() * 3)]
+    winnerCheck = checkWinner(userSelect, compSelect)
+    scoreUser.textContent = localStorage.getItem("userScore")
+    scoreComp.textContent = localStorage.getItem("compScore")
+    resultPage = resultsPageContent(userSelect, compSelect, winnerCheck)
+    resultsPageRender(resultPage)
+  }
+
+  if (e.target.classList.contains("reset-btn")) {
+    resetRender()
+  }
+  if (e.target.classList.contains("next-btn")) {
+    nextWinner(winnerCheck)
+  }
+  if (e.target.classList.contains("win-reset-btn")) {
+    initialPageRender()
   }
 })
